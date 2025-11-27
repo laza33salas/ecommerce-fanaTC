76c2b793f923b82e2770a458c7c7b6c73601077e
Lazaro Salas <lazarocode@gmail.com>
2025-11-27 15:38:36 -0300

    Archivo Common/BaseDomainModel.cs

---
+++ backend/src/Core/Ecommerce.Domain/Common/BaseDomainModel.cs
@@ -0,0 +1,18 @@
+namespace Ecommerce.Domain.Common;
+
+public abstract class BaseDomainModel
+{
+    public int Id { get; set; }
+
+//Para auditoria
+    public DateTime? CreatedDate { get; set; }
+
+//¿Quien lo creó?
+    public string? CreatedBy { get; set; }
+
+//¿Cuando se modificó por última vez?
+    public DateTime? LastModifiedDate { get; set; }
+
+//¿Quien lo modificó por última vez?
+    public DateTime? LastModifiedBy { get; set; }
+}
